f3d2eb53860911160ec8b31352b0ddd619f880f8
Zohaib Wasim <zohaib.wasim@gmail.com>
2022-01-07 11:07:46 -0500

    added animations for project focus

--- src/app/components/projectile/projectile.component.ts
+++ src/app/components/projectile/projectile.component.ts
@@ -15,6 +15,8 @@ export class ProjectileComponent implements OnInit, AfterViewInit{
   @ViewChild('desc') desc: ElementRef<HTMLSpanElement>;
   @ViewChild('projectile') projectile: ElementRef<HTMLDivElement>;
   @ViewChild('text') text: ElementRef<HTMLParagraphElement>;
+  @ViewChild('display') display: ElementRef<HTMLImageElement>;
+  
   showdesc;
   hidedesc;
   restartanim = true;
@@ -28,14 +30,17 @@ export class ProjectileComponent implements OnInit, AfterViewInit{
   ngAfterViewInit(): void {
     this.projectile.nativeElement.onmouseenter = (ev) => {
         this.desc.nativeElement.style.top = "0%";
-        this.desc.nativeElement.style.opacity = "1";
+        this.desc.nativeElement.style.backgroundColor = "rgba(102, 102, 102, 0.8)";
         this.text.nativeElement.style.opacity = "1";
+        this.display.nativeElement.style.filter = "blur(3px)";
+        this.display.nativeElement.style.transform = "scale(1.2)";
     }
 
     this.projectile.nativeElement.onmouseleave = (ev) => {
       this.desc.nativeElement.style.top = "100%";
-      this.desc.nativeElement.style.opacity = "1";
       this.text.nativeElement.style.opacity = "0";
+      this.display.nativeElement.style.filter = "blur(0px)";
+        this.display.nativeElement.style.transform = "scale(1)";
     }
   }
 
